de329496ff2e804e853c208c48cba86b5056ccd9
lucas <lucas.chorolqui@gmail.com>
2023-10-21 22:29:08 -0300

    [Config] adding latest features

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,14 @@ import CreateOrder, {
 import Order, { loader as orderLoader } from "./features/order/Order";
 import AppLayout from "./ui/AppLayout";
 import Error from "./ui/Error";
+import { action as updateOrderAction } from "./features/order/UpdateOrder";
+
+// Add: sent an email when user order, option to change some value from orderP page,
+// 1) On order, you could let the user define a PIN number for the order. Then, in the page that displays the final order, the user could be allowed to actually edit the order during the first 5 minutes after submitting it, but ONLY if they input the correct PIN number (otherwise, users could edit orders from other people). This would require a PIN to be sent to the API request on order.
+
+// 2) You could add the ability to add or remove ingredients for pizzas in the cart. Again, this would require a change in the API.
+
+// 3) You could persist the cart data using "Redux persist" like in this tutorial: https://blog.logrocket.com/persist-state-redux-persist-redux-toolkit-react/
 
 const router = createBrowserRouter([
   {
@@ -40,6 +48,7 @@ const router = createBrowserRouter([
         element: <Order />,
         loader: orderLoader,
         errorElement: <Error />,
+        action: updateOrderAction,
       },
     ],
   },
--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -1,17 +1,24 @@
 import OrderItem from "./OrderItem";
 
-import { useLoaderData } from "react-router-dom";
+import { useFetcher, useLoaderData } from "react-router-dom";
 import { getOrder } from "../../services/apiRestaurant";
 import {
   calcMinutesLeft,
   formatCurrency,
   formatDate,
 } from "../../utils/helpers";
+import { useEffect } from "react";
+import UpdateOrder from "./UpdateOrder";
 // Test ID: IIDSAT
 
 function Order() {
   // order recive data from loader(), previus pased in App component as "orderLoader",
   const order = useLoaderData();
+
+  const fetcher = useFetcher();
+  useEffect(() => {
+    if (!fetcher.data && fetcher.state === "idle") fetcher.load("/menu");
+  }, [fetcher]);
 
   // Everyone can search for all orders, so for privacy reasons we're gonna gonna exclude names or address, these are only for the restaurant staff
   const {
@@ -56,7 +63,15 @@ function Order() {
 
       <ul className="dive-stone-200 divide-y border-b border-t">
         {cart.map((item) => (
-          <OrderItem item={item} key={item.id} />
+          <OrderItem
+            item={item}
+            key={item.id}
+            isLoadingIngredients={fetcher.state === "loading"}
+            ingredients={
+              fetcher?.data?.find((el) => el.id === item.pizzaId)
+                ?.ingredients ?? []
+            }
+          />
         ))}
       </ul>
 
@@ -73,6 +88,7 @@ function Order() {
           To pay on delivery: {formatCurrency(orderPrice + priorityPrice)}
         </p>
       </div>
+      {!priority && <UpdateOrder order={order} />}
     </div>
   );
 }
